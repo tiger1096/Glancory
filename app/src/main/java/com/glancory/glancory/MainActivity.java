4df35012c8fef5907759eaa50b381e25c5183ce8
tiger1096 <1261240928@qq.com>
2018-09-13 23:45:43 +0800

    初步支持设置锁屏背景图片

--- app/src/main/java/com/glancory/glancory/MainActivity.java
+++ app/src/main/java/com/glancory/glancory/MainActivity.java
@@ -1,13 +1,54 @@
 package com.glancory.glancory;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.glancory.glancory.receiver.ScreenStatusReceiver;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScreenStatusReceiver mScreenStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScreenStatusReceiver = new ScreenStatusReceiver(this);
+        IntentFilter mIntentFilter = new IntentFilter();
+
+        mIntentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        mIntentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+//        mIntentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+//        mIntentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+
+        Intent i = registerReceiver(mScreenStatusReceiver, mIntentFilter);
+        Log.e("ha","Intent.ACTION_SCREEN_ON");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(mScreenStatusReceiver);
+        Log.e("ha","unIntent.ACTION_SCREEN_ON");
+
     }
 }
---
+++ app/src/main/java/com/glancory/glancory/receiver/ScreenStatusReceiver.java
@@ -0,0 +1,76 @@
+package com.glancory.glancory.receiver;
+
+import android.app.WallpaperManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+public class ScreenStatusReceiver extends BroadcastReceiver {
+
+    Context mContext;
+
+    public ScreenStatusReceiver(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        System.out.println("ScreenStatusReceiver receive " + intent.getAction());
+
+        Log.e("ScreenStatusReceiver", "ScreenStatusReceiver receive " + intent.getAction());
+
+        if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            SetLockWallPaper();
+        }
+
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void SetLockWallPaper() {
+        // TODO Auto-generated method stub
+        try {
+            WallpaperManager mWallManager = WallpaperManager.getInstance(mContext);
+            mWallManager.setBitmap(getImageFromAssetsFile("bg1.jpg"), null, false, WallpaperManager.FLAG_LOCK);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block e.printStackTrace();
+
+            Log.e("ScreenStatusReceiver", "ScreenStatusReceiver receive fail " + e.getMessage() + ", and name = " + e.getClass().getName());
+
+        }
+    }
+
+    private Bitmap getImageFromAssetsFile(String fileName)
+    {
+        Bitmap image = null;
+        AssetManager am = mContext.getResources().getAssets();
+        try
+        {
+            InputStream is = am.open(fileName);
+            image = BitmapFactory.decodeStream(is);
+            is.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return image;
+
+    }
+}
