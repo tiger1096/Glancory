d0a87ad7fd12e7ca3e519064ac122aaaca14ad61
tiger1096 <1261240928@qq.com>
2018-11-05 22:58:30 +0800

    添加客户端登录注册页面

--- app/src/main/java/com/glancory/glancory/MainActivity.java
+++ app/src/main/java/com/glancory/glancory/MainActivity.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.glancory.glancory.receiver.BootReceiver;
 
+import java.net.HttpURLConnection;
+
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
+    private Button logupBtn;
+    private EditText user_input;
+    private EditText password_input;
+
     private BootReceiver mBootReveiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         mBootReveiver = new BootReceiver();
         IntentFilter intent = new IntentFilter();
@@ -21,6 +32,53 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(mBootReveiver, intent);
 
         sendBroadcast(new Intent("RestartSerivcesForSystemEventReceiver"));
+
+        initLoginLayout();
+    }
+
+    public void initLoginLayout() {
+        loginBtn = findViewById(R.id.login_btn);
+        logupBtn = findViewById(R.id.logup_btn);
+        user_input = findViewById(R.id.user_name);
+        password_input = findViewById(R.id.user_password);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName = user_input.getText().toString().trim();
+                String password = password_input.getText().toString().trim();
+
+                if(userName == null || userName.equals("") || password == null || password.equals("")) {
+                    Toast.makeText(MainActivity.this, "用户名和密码不能为空", Toast.LENGTH_SHORT);
+                    return;
+                }
+
+                boolean ret = login(userName, password);
+                if (!ret) {
+                    Toast.makeText(MainActivity.this, "登录失败，请重新登录", Toast.LENGTH_SHORT);
+                    return;
+                } else {
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    public boolean login(String userName, String password) {
+
+        new Thread(){
+            private HttpURLConnection httpConnect;
+
+            @Override
+            public void run() {
+                super.run();
+            }
+        }.start();
+
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/glancory/glancory/Utils/HttpConnectionUtils.java
@@ -0,0 +1,8 @@
+package com.glancory.glancory.Utils;
+
+/**
+ * Created by zhangqi on 18/11/5.
+ */
+
+public class HttpConnectionUtils {
+}
